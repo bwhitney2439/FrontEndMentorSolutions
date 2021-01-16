c28868199b0ce4376cb155b306781cb797e367ec
Ben Whitney <ben.whitn@gmail.com>
2021-01-16 22:07:36 +0000

    asdfghbm

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -1,3 +1,5 @@
+const draggables = document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".todos");
 const todosContainer = document.querySelector(".todos");
 const todosFooter = document.querySelector(".todos-footer");
 const todosFooterMobile = document.querySelector(".todos-footer-mobile");
@@ -79,6 +81,39 @@ createTodoCheckradio.addEventListener("click", (event) => {
   }
 });
 
+containers.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
+});
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.dragging)"),
+  ];
+
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
+}
+
 // Create Todo
 const createTodo = (id, todoString) => {
   const todoLabel = document.createElement("label");
@@ -86,6 +121,15 @@ const createTodo = (id, todoString) => {
   todoLabel.classList.add("todo-label");
   const todo = document.createElement("div");
   todo.classList.add("todo");
+  todo.classList.add("draggable");
+  todo.draggable = true;
+  todo.addEventListener("dragstart", () => {
+    todo.classList.add("dragging");
+  });
+
+  todo.addEventListener("dragend", () => {
+    todo.classList.remove("dragging");
+  });
 
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
@@ -136,7 +180,7 @@ const createTodo = (id, todoString) => {
   crossIconContainer.appendChild(crossIcon);
   todo.appendChild(crossIconContainer);
 
-  todosContainer.insertBefore(todo, todosFooter);
+  todosContainer.appendChild(todo);
 };
 
 const clearCompleted = () => {
